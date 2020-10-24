a7443145ce21cc4db9f5e170de8a83049a6e742b
fourside <fourside@gmail.com>
2020-10-24 22:12:37 +0900

    s3 and lambda

--- lib/podcast-resources-stack.ts
+++ lib/podcast-resources-stack.ts
@@ -1,9 +1,40 @@
-import * as cdk from "@aws-cdk/core";
+import { Stack, StackProps, Construct } from "@aws-cdk/core";
+import { Bucket } from "@aws-cdk/aws-s3";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import { Runtime } from "@aws-cdk/aws-lambda";
+import { Rule, Schedule } from "@aws-cdk/aws-events";
+import { LambdaFunction } from "@aws-cdk/aws-events-targets";
+import { RetentionDays } from "@aws-cdk/aws-logs";
 
-export class PodcastResourcesStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+const PARCEL_CACHE_BASE_DIR = "./parcelCache";
+export class PodcastResourcesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new Bucket(this, "radiko-programs", {
+      bucketName: "radiko-programs",
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        ignorePublicAcls: true,
+        blockPublicPolicy: true,
+        restrictPublicBuckets: true,
+      },
+    });
+
+    const xmlProcessorFunction = new NodejsFunction(this, "xmlProcessor", {
+      entry: "lambda/xmlProcessor/src/index.ts",
+      handler: "handler",
+      runtime: Runtime.NODEJS_12_X,
+      cacheDir: `${PARCEL_CACHE_BASE_DIR}/xml`,
+      logRetention: RetentionDays.ONE_MONTH,
+    });
+
+    new Rule(this, "radikoXml", {
+      schedule: Schedule.cron({
+        minute: "0",
+        hour: "0",
+      }),
+      targets: [new LambdaFunction(xmlProcessorFunction)],
+    });
   }
 }
