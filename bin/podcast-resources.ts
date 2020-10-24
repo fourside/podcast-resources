75cada728e02d3d19dc6539a5ae0574fcf19c550
fourside <fourside@gmail.com>
2020-10-24 17:44:57 +0900

    Initial commit

---
+++ bin/podcast-resources.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { PodcastResourcesStack } from '../lib/podcast-resources-stack';
+
+const app = new cdk.App();
+new PodcastResourcesStack(app, 'PodcastResourcesStack');
---
+++ test/podcast-resources.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as PodcastResources from '../lib/podcast-resources-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new PodcastResources.PodcastResourcesStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
