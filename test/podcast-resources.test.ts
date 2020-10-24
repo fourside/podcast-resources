669b5b8273d38d28d896e8c09a79f9f5e2fad15d
fourside <fourside@gmail.com>
2020-10-24 21:00:32 +0900

    prettier

--- test/podcast-resources.test.ts
+++ test/podcast-resources.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as PodcastResources from '../lib/podcast-resources-stack';
+import { expect as expectCDK, matchTemplate, MatchStyle } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as PodcastResources from "../lib/podcast-resources-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PodcastResources.PodcastResourcesStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new PodcastResources.PodcastResourcesStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
